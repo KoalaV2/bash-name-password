70391c6358738c2f99482b2ad0a2cf95a7903300
KoalaV2 <theo.johanson@gmail.com>
2019-02-25 15:02:50 +0100

    Added input for the Ãpassword

--- fun.sh
+++ fun.sh
@@ -3,7 +3,12 @@ echo "Hello! What's your name?"
 read name
 if [[ "$name" == koala ]]; then
         echo "Hello "$name"!"
+        echo "What is the password?"
+        read -s password
+        if [[ "$password" == testing123 ]]; then
+                echo "Correct password!"
+                neofetch && echo "Welcome koala"
+	fi
 else
         echo "Hello $(whoami)"
 fi
-
